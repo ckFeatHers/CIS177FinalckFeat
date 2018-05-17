8f94cbf75dfbfbeeca8f367f9fc881ed5f6f7993
ckFeatHers <36851462+ckFeatHers@users.noreply.github.com>
2018-05-16 21:58:05 -0500

    Submitted Final
    For review only.  
    Adjusted the onclick to clear output for resetting info.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,42 @@
-/** *************** PART 'DONE'  SET DATA POINTS  **********************
- * These need to be global
+/** *************** FINAL PROJECT CIS-177 SP18  **********************
+ *                  CHARLOTTE FEATHERS 16 MAY
  */
 
+// global varibles needed:
 let userPts = 1;
 let goal;
 let classId;
 let week;
-// for class obj
+
+// for class obj:
 let myClass;
 
-/** Sets the 'myClass' to use for output */
+// buttons
+const doneBtn = document.querySelector("#done-btn");
+const trackBtn = document.querySelector("#track-btn");
+const marginBtn = document.querySelector("#margin-btn");
+
+/** ********  INITIALIZING : DONE BUTTON  ************************/
+
+/** Retrieves; Tests; Assigns Data Fields.
+ * Retrieves user Data from input values
+ * Tests if valid data/gives alert if Not
+ * Sets the 'myClass' to use for output  *
+ */
 function clickHandlerDone() {
+  // clears output fields ready for new data
+  clearOutput();
+
+  // retrieves and tests
   userPts = parseInt(document.querySelector("#user-pts").value);
+
   if (userPts > 0) {
+    // Retrieves remaining
     goal = parseInt(document.querySelector("#goal").value);
     classId = document.querySelector("#course").value;
     week = parseInt(document.querySelector("#week-num").value);
-    console.log("line 17 inside clickHandlerDone " + userPts);
 
+    // assigns values
     let myClassNew = {
       className: courseMap.get(classId).className,
       scale: courseMap.get(classId).scale,
@@ -26,32 +45,36 @@ function clickHandlerDone() {
       goalPts: getGoalPts(classId, goal)
     };
 
+    // gives global myClass the new input values
     myClass = myClassNew;
+    classGoal();
     console.log(myClass);
   } else {
-    alert("WARNING: enter current points for this class before continuing.");
-  }
-  /** here I would like to use #output in some way
-  to cause the form to show. I assume using
+    /** here I would like to use #output in some way
+  to cause the output form to appear. I assume using
   the template tag and replacing would work best.
   document.querySelector("#output")
   could also simulate the buttons are inactive with color change
   UNTIL THEN GIVE WARNING THAT DATA NEEDS ENTERING.
-   */
+  */
+    alert("WARNING: enter current points for this class before continuing.");
+  }
+}
 
-  // CLEAR THE OUTPUT FIELDS WHEN NEW CLASS IS GENERATED
+doneBtn.addEventListener("click", clickHandlerDone);
+
+/** ****************    METHODS  USED ************************/
+
+/** Clear output Fields
+ */
+function clearOutput() {
   document.querySelector("#out-Goal").textContent = "";
   document.querySelector("#out-Track").textContent = "";
   document.querySelector("#out-Margin").textContent = "";
 }
-const doneBtn = document.querySelector("#done-btn");
-doneBtn.addEventListener("click", clickHandlerDone);
 
-/** **********************   PART I  ******************************/
-// use methods to create remaining two values for obj course myClass
-
-/** Max Points Available fxn
- * 2 refrences
+/** Max Points Available USED for obj myClass attribute
+ *
  * @param {String} id class Id
  * @param {Number} wk week num; total 17
  * @return {Number} max
@@ -62,11 +85,10 @@ function getTotalPts(id, wk) {
   for (let i = 0; i < wk; i++) {
     total += ptsArray[i];
   }
-  console.log(total);
   return total;
 }
 
-/** Target Points
+/** Target Points Needed USED for obj myClass attribute
  * @param {Number} id class max
  * @param {Number} X goal; facotr of scale
  * @return {Number} goal points for user in class
@@ -76,46 +98,16 @@ function getGoalPts(id, X) {
   const sc = courseMap.get(classId).scale;
 
   const pts = Math.ceil(max * (100 - sc * X) / 100);
-
   return pts;
 }
 
-/** **********************   PART II  ******************************/
-// Methods get percent and get Goal points. output click handler: see dom.js
-
-/** Get the Percentage
- * @param {Number} a user current points ; pts need
- * @param {Number} B class current max pts; pts remaining
- * @return {Number} percent avg for output
+/** Creates Output of Class's Name and Goal Points
+ * need: userPts and class's goalPts
+ * @return {void} Outputs to browser;
+ * Dev note: this used to be the Goal button
  */
-function getPercent(a, B) {
-  const percent = Math.floor(1000 * a / B) / 10;
-
-  return percent;
-}
-
-/** **********************   PART III  ******************************/
-// Method needed: Percentage from partII, Difference. Click Handler.
-
-/** Get the Difference
- * @param {Number} pig GoalPts; maxPts; RemainClass
- * @param {Number} goat user-pts; wkMax; RemainNeed
- * @return {Number} Difference of two numbers
- */
-function getDif(pig, goat) {
-  const weight = pig - goat;
-
-  return weight;
-}
-
-/** ****************************************************************** */
-/** PART I Click Handler
- * userPts and class's goalPts needed.
- * @return {void} void; out put of class name and goalPts calculcated from getGoalPts()
- */
-function clickHandlerSet() {
+function classGoal() {
   const gp = myClass.goalPts;
-
   if (gp > 0) {
     any = `${myClass.className} has a total of ${
       myClass.maxPts
@@ -125,14 +117,33 @@ function clickHandlerSet() {
   }
   document.querySelector("#out-Goal").textContent = any;
 }
-const classGoalBtn = document.querySelector("#goal-btn");
-classGoalBtn.addEventListener("click", clickHandlerSet);
 
-/** ****************************************************************** */
+/** Get the Percentage
+ * @param {Number} a user current points ; pts need
+ * @param {Number} B class current max pts; pts remaining
+ * @return {Number} percent avg for output
+ */
+function getPercent(a, B) {
+  const percent = Math.floor(1000 * a / B) / 10;
+  return percent;
+}
 
-/** Part II Click Handler
+/** Get the Difference
+ * @param {Number} pig GoalPts; maxPts; RemainClass
+ * @param {Number} goat user-pts; wkMax; RemainNeed
+ * @return {Number} Difference of two numbers
+ */
+function getDif(pig, goat) {
+  const weight = pig - goat;
+  return weight;
+}
+
+/** *************************  DOM   **************************** */
+
+/** Click Handler Track %  
+const trackBtn = document.querySelector("#track-btn");
  * To track; need userPts, weekPts(the total to date)
- * @return {void} inserts results to DOM
+ * @return {void} outputs to browser
  */
 function clickHandlerTrack() {
   let track;
@@ -145,16 +156,14 @@ function clickHandlerTrack() {
 
   document.querySelector("#out-Track").textContent = track;
 }
-const trackBtn = document.querySelector("#track-btn");
 trackBtn.addEventListener("click", clickHandlerTrack);
-/** ****************************************************************** */
 
-/** Part III click Handler
+/**  Click Handler Margin *************
  * -Margin: How many pts you need for goal and how many pts reamin
- * -% for remaining in class
- * @return {void} out put to browser
+ * // deleted option for % of remaining assignments.
+ * @return {void} outputs to browser
  */
-function clickHandler() {
+function clickHandlerMargin() {
   const need = getDif(myClass.goalPts, userPts);
   const remain = getDif(myClass.maxPts, myClass.weekPts);
   const margin = getDif(remain, need);
@@ -165,12 +174,11 @@ function clickHandler() {
   } else if (margin == 0) {
     marginMsg = `You have NO room for error for this goal.  You may want to reconsider.`;
   } else if (margin < 0) {
-    marginMsg = `You have missed your goal by ${margin *
+    marginMsg = `You are off your goal by ${margin *
       -1}. A new goal is needed!`;
   } else {
-    marginMsg = `Not sure of your status. Please check blackboard.`;
+    marginMsg = `Not sure of your status. Check input the items and select Done.`;
   }
   document.querySelector("#out-Margin").textContent = marginMsg;
 }
-const marginBtn = document.querySelector("#margin-btn");
-marginBtn.addEventListener("click", clickHandler);
+marginBtn.addEventListener("click", clickHandlerMargin);
